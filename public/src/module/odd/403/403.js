c335fa98d16f92b4494ad1ab1779db54705c8dea
shuchao <645904877@qq.com>
2017-11-12 01:20:56 +0800

    fix bug

--- public/src/module/odd/403/403.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Odd from '../../../component/Odd';
-
-export default ({props}) => {
-    return (
-        <Odd type="403" {...props}/>
-    );
-};
--- public/src/module/odd/500/500.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Odd from '../../../component/Odd';
-
-export default ({props}) => {
-    return (
-        <Odd type="500" actions {...props}/>
-    );
-};
