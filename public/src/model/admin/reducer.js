db1e819ec788b26c83e69ddfc8874d81ea6974e7
shuchao <645904877@qq.com>
2018-01-13 00:42:55 +0800

    ...

--- public/src/model/admin/reducer.js
+++ public/src/model/admin/reducer.js
@@ -1,5 +1,31 @@
 //noinspection JSUnusedGlobalSymbols
 export default (state = {
+    menu    : {
+        home  : {
+            name    : '管理后台',
+            icon    : 'home',
+            children: {
+                dashboard: {
+                    name: '后台首页',
+                    icon: 'dashboard',
+                }
+            }
+        },
+        secure: {
+            name    : '安全中心',
+            icon    : 'secure',
+            children: {
+                account  : {
+                    name: '账号管理',
+                    icon: 'account',
+                },
+                authorize: {
+                    name: '权限管理',
+                    icon: 'authorize',
+                },
+            }
+        },
+    },
     username: '舒超',
 }, action) => {
     switch (action.type) {
--- public/src/module/admin/admin.js
+++ public/src/module/admin/admin.js
@@ -1,34 +1,85 @@
 import React from 'react';
+import ClassNames from 'classnames';
+import {Layout, Menu, Icon, Avatar, Dropdown, Tag, message, Spin} from 'antd';
+import {ContainerQuery} from 'react-container-query';
 import DocumentTitle from 'react-document-title';
 
 import Inject from '../inject';
 
+import style from './admin.less';
+
+const query = {
+    'screen-xs': {
+        maxWidth: 575,
+    },
+    'screen-sm': {
+        minWidth: 576,
+        maxWidth: 767,
+    },
+    'screen-md': {
+        minWidth: 768,
+        maxWidth: 991,
+    },
+    'screen-lg': {
+        minWidth: 992,
+        maxWidth: 1199,
+    },
+    'screen-xl': {
+        minWidth: 1200,
+    },
+};
+
+const {Sider, Header, Content} = Layout;
+
 class Admin extends React.Component {
 
-    getPageTitle(menu = {
-
-    }) {
+    getPageTitle(menu) {
         const {location} = this.props;
-        const {pathname} = {location};
-        let item = Object.assign({}, menu);
-        pathname.split('/').substr(0, 2).forEach(name => {
-            item = item[name] ? item[name] : {};
-        });
-
-
-        console.log(pathname);
-        return '管理后台';
+        let unique = location.pathname.split('/').slice(0, 3).join('/');
+        const traverse = (menu, father = '', map = {}) => {
+            Object.keys(menu).forEach(route => {
+                let unique = `${father}/${route}`;
+                typeof menu[route].children !== 'object' ? (map[unique] = menu[route].name) : traverse(menu[route].children, unique, map);
+            });
+            return map;
+        };
+        return traverse(menu)[unique] || '管理后台';
     }
 
     render() {
         const {dispatch, state} = this.props;
 
+        const layout = (
+            <Layout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    breakpoint="md"
+                    width={256}
+                    style={ClassNames(style.sider)}
+                >
+
+                </Sider>
+                <Layout>
+                    <Header>
+
+                    </Header>
+                    <Content style={{margin: '24px 24px 0', height: '100%'}}>
+                        <div>
+                            <span>{state.username}</span>
+                            <button onClick={() => dispatch('change', {username: 'Shuc324@gmail.com'})}>改变名字</button>
+                        </div>
+                    </Content>
+                </Layout>
+            </Layout>
+        );
+
+
         return (
             <DocumentTitle title={this.getPageTitle(state.menu || {})}>
-                <div>
-                    <span>{state.username}</span>
-                    <button onClick={() => dispatch('change', {username: 'Shuc324@gmail.com'})}>改变名字</button>
-                </div>
+                <ContainerQuery query={query}>
+                    {params => <div className={ClassNames(params)}>{layout}</div>}
+                </ContainerQuery>
             </DocumentTitle>
         );
     }
