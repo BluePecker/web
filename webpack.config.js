8635ecea364439f6fc970e8f95b0cd77e71c0532
shuchao <645904877@qq.com>
2018-01-18 10:18:04 +0800

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -128,7 +128,7 @@ module.exports = {
             filename: 'index.html',
             favicon : './public/favicon.ico'
         }),
-        new webpack.optimize.UglifyJsPlugin(),
+        // new webpack.optimize.UglifyJsPlugin(),
         new extracttextplugin("asset/css/[hash].css"),
         new manifestplugin('build/manifest.json'),
         new webpack.optimize.OccurrenceOrderPlugin(),
