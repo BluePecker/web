ead43a5f67909560492c134305574d2644c29a18
shuchao <645904877@qq.com>
2018-01-24 17:09:42 +0800

    ...

--- public/src/model/model.js
+++ public/src/model/model.js
@@ -2,20 +2,27 @@ import * as Redux from 'redux';
 
 const reducers = Redux.combineReducers((() => {
     const container = {};
-    const reducers = require.context('./', true, /\/reducer\.js$/).keys();
-    reducers.forEach(reducer => {
-        const item = reducer.replace(/\/reducer\.js$/, '');
-        const namespace = item.substring(2);
 
-        const init = require(`${reducer}`).state || {};
-        const func = require(`${reducer}`).default || {};
+    const reducers = require.context('./', true, /\/$/).keys();
+    reducers.forEach(model => {
+        const namespace = model.replace(/(^\.\/|\/$)/g, '');
+        model = require(`${model}`).default;
 
-        container[namespace] = (state = typeof init === 'function' ? init() : init, action) => {
-            const method = !action.type ? '' : action.type.replace(`${namespace}/`, '');
-            const payload = Object.assign({}, action);
-            delete payload['type'];
-            return Reflect.has(func, method) && typeof func[method] === 'function' ? func[method](state, payload) : state;
-        };
+        if (typeof model === 'function') {
+            const reducer = new (model().Reducer);
+            container[namespace] = (state, action) => {
+                const {type} = action;
+                const method = (type || '').replace(`${namespace}/`, '');
+
+                return Reflect.has(reducer, method) ? Reflect.apply() : state;
+            };
+
+
+            // container[namespace] = (state, action) => {
+            // const method = !action.type ? '' : action.type.replace(`${namespace}/`, '');
+            // reducer.ownKeys(reducer).indexOf(method) > -1 && Reflect.apply(reducer[method], reducer, [state, action]);
+            // };
+        }
     });
     return container;
 })());
---
+++ public/src/model/user/login/index.js
@@ -0,0 +1,12 @@
+export default (state = {}, dispatch) => {
+
+    class Reducer {
+
+    }
+
+    class Dispatch {
+
+    }
+
+    return {Reducer: Reducer, Dispatch: Dispatch};
+};
